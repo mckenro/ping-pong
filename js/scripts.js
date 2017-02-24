d61a81fa312bd7453b3b2b8612798e4a3f526ddd
Rob McKenzie <rob@>
2017-02-24 15:46:15 -0800

    add finished project

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@
 var pingPongCounter = function(number) {
   var result = [];
   var number = parseInt(number);
-  var start = 1;
-  for (i=0;i<number;i++) {
+  var start = 0;
+  for (i=0;i<=number;i++) {
     result.push(start++);
   }
-  result.map(function(index) {
+  result.forEach(function(index) {
     if (result[index] % 15 === 0){
       result[index] = "ping-pong";
     }else if (result[index] % 5 === 0){
